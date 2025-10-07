7a0c1da0c0d7dffa24557234c976d9c11a771111
Djalma <djalmahy@hotmail.com>
2025-10-07 17:04:36 -0300

    Fix: frontend dependecies.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0'
-  }
-})
-
+    plugins: [react(), tailwindcss()],
+    base: '/secure-finances-app/',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
